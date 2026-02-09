0ef66586e3daa0e20fb04104d21db8ad43b40f0a
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-02-10 02:43:34 +0300

    Update gadget-entry.js

--- src/gadget-entry.js
+++ src/gadget-entry.js
@@ -1,9 +1,5 @@
 // <nowiki>
 
-if (typeof categoryBatchManager === 'undefined') {
-    var categoryBatchManager = {};
-}
-
 function newFunction(require, target_id) {
     const Vue = require('vue');
     const Codex = require('@wikimedia/codex');
@@ -27,7 +23,6 @@ function newFunction(require, target_id) {
 mw.loader.using(['@wikimedia/codex', 'mediawiki.api', 'vue']).then(function (require) {
     const target = document.getElementById('category-batch-manager2');
     if (target) {
-        categoryBatchManager.api = new mw.Api();
         newFunction(require, '#category-batch-manager2');
     } else {
         // Check if we're on a category page
@@ -45,8 +40,9 @@ mw.loader.using(['@wikimedia/codex', 'mediawiki.api', 'vue']).then(function (req
         const mountPoint = document.body.appendChild(document.createElement('div'));
         mountPoint.id = 'category-batch-manager2';
         mountPoint.style.display = 'none';
-        categoryBatchManager.api = new mw.Api();
+
         newFunction(require, '#category-batch-manager2');
+
         portletLink.addEventListener('click', function (e) {
             e.preventDefault();
             mountPoint.style.display = 'block';
