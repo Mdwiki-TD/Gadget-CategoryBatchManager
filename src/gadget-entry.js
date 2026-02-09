eb53dedc1a47f21110dbc352b85f12c5a0e87f35
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-02-10 02:36:34 +0300

    Update gadget-entry.js

--- src/gadget-entry.js
+++ src/gadget-entry.js
@@ -4,12 +4,7 @@ if (typeof categoryBatchManager === 'undefined') {
     var categoryBatchManager = {};
 }
 
-mw.loader.using(['@wikimedia/codex', 'mediawiki.api', 'vue']).then(function (require) {
-    const target = document.getElementById('category-batch-manager2');
-    if (!target) {
-        return;
-    }
-    categoryBatchManager.api = new mw.Api();
+function newFunction(require, target_id) {
     const Vue = require('vue');
     const Codex = require('@wikimedia/codex');
 
@@ -26,7 +21,36 @@ mw.loader.using(['@wikimedia/codex', 'mediawiki.api', 'vue']).then(function (req
         .component('cdx-dialog', Codex.CdxDialog)
         .component('cdx-label', Codex.CdxLabel)
         .component('cdx-multiselect-lookup', Codex.CdxMultiselectLookup)
-        .mount('#category-batch-manager2');
+        .mount(target_id);
+}
+
+mw.loader.using(['@wikimedia/codex', 'mediawiki.api', 'vue']).then(function (require) {
+    const target = document.getElementById('category-batch-manager2');
+    if (target) {
+        categoryBatchManager.api = new mw.Api();
+        newFunction(require, '#category-batch-manager2');
+    } else {
+        // Check if we're on a category page
+        var isCategoryPage = mw.config.get('wgCanonicalNamespace') === 'Category';
+        if (!isCategoryPage) return;
+
+        // Add button to page
+        var portletLink = mw.util.addPortletLink(
+            'p-cactions',
+            '#',
+            'Batch Manager',
+            'ca-batch-manager',
+            'Open Category Batch Manager'
+        );
+        const mountPoint = document.body.appendChild(document.createElement('div'));
+        mountPoint.id = 'category-batch-manager2';
+        newFunction(require, '#category-batch-manager2');
+        portletLink.addEventListener('click', function (e) {
+            e.preventDefault();
+            mountPoint.style.display = 'block';
+        });
+
+    }
 });
 
 // </nowiki>
