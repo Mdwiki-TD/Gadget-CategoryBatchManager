bc3444f5eeb7c7bdab89bc59cd64c27c010029f4
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-02-11 04:49:05 +0300

    Use default exports in unit tests
    Replace CommonJS require(...) calls with destructured { default: ... } imports in multiple unit tests to match modules that export default. Updated files: tests/unit/services/CategoryService.test.js, tests/unit/services/SearchService.test.js, tests/unit/utils/CategoryNormalization.test.js, tests/unit/utils/Validator.test.js, tests/unit/utils/WikitextParser.test.js. Also assign globals (global.WikitextParser and global.FileModel) where needed so services under test can access these dependencies.

--- tests/unit/utils/Validator.test.js
+++ tests/unit/utils/Validator.test.js
@@ -1,4 +1,4 @@
-const Validator = require('../../../src/utils/Validator');
+const { default: Validator } = require('../../../src/utils/Validator');
 
 describe('Validator', () => {
   describe('isValidCategoryName', () => {
