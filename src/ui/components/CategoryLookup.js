379aa7b8684319144f2101e37021ec05c57a1cdf
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-02-11 03:34:56 +0300

    Update CategoryLookup.js

--- src/ui/components/CategoryLookup.js
+++ src/ui/components/CategoryLookup.js
@@ -1,4 +1,3 @@
-
 function CategoryLookupComponent() {
     return {
         props: {
@@ -11,31 +10,45 @@ function CategoryLookupComponent() {
         },
 
         template: `
-            <div class="cbm-category-input-group">
-                <cdx-label class="cbm-label">
-                    {{ label }}
-                </cdx-label>
+            <div>
+                <div class="cbm-category-input-group">
+                    <cdx-label class="cbm-label">
+                        {{ label }}
+                    </cdx-label>
 
-                <cdx-multiselect-lookup
-                    v-model:input-chips="model.chips"
-                    v-model:selected="model.selected"
-                    v-model:input-value="model.input"
-                    :menu-items="model.menuItems"
-                    :menu-config="model.menuConfig"
-                    :aria-label="ariaLabel"
-                    :placeholder="placeholder"
-                    @update:input-value="onInput"
-                    @load-more="onLoadMore"
-                >
-                    <template #no-results>
-                        Type at least 2 characters to search
-                    </template>
-                </cdx-multiselect-lookup>
+                    <cdx-multiselect-lookup
+                        v-model:input-chips="model.chips"
+                        v-model:selected="model.selected"
+                        v-model:input-value="model.input"
+                        :menu-items="model.menuItems"
+                        :menu-config="model.menuConfig"
+                        :aria-label="ariaLabel"
+                        :placeholder="placeholder"
+                        @update:input-value="onInput"
+                        @load-more="onLoadMore"
+                    >
+                        <template #no-results>
+                            Type at least 2 characters to search
+                        </template>
+                    </cdx-multiselect-lookup>
+                </div>
+
+                <div v-if="model.message.show" class="margin-bottom-20">
+                    <cdx-message
+                        allow-user-dismiss
+                        :type="model.message.type"
+                        :inline="false"
+                    >
+                        {{ model.message.text }}
+                    </cdx-message>
+                </div>
             </div>
         `,
 
         methods: {
             async onInput(value) {
+                this.model.message.show = false;
+
                 const data = await this.handler.onCategoryInput(
                     value,
                     this.model.input,
