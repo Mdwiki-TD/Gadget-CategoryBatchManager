56f3520e7d7385fd91a8245dab923c1836f8babf
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-02-11 03:33:03 +0300

    1

---
+++ src/ui/components/CategoryLookup.js
@@ -0,0 +1,59 @@
+
+function CategoryLookupComponent() {
+    return {
+        props: {
+            model: Object,
+            label: String,
+            ariaLabel: String,
+            placeholder: String,
+            type: String,
+            handler: Object
+        },
+
+        template: `
+            <div class="cbm-category-input-group">
+                <cdx-label class="cbm-label">
+                    {{ label }}
+                </cdx-label>
+
+                <cdx-multiselect-lookup
+                    v-model:input-chips="model.chips"
+                    v-model:selected="model.selected"
+                    v-model:input-value="model.input"
+                    :menu-items="model.menuItems"
+                    :menu-config="model.menuConfig"
+                    :aria-label="ariaLabel"
+                    :placeholder="placeholder"
+                    @update:input-value="onInput"
+                    @load-more="onLoadMore"
+                >
+                    <template #no-results>
+                        Type at least 2 characters to search
+                    </template>
+                </cdx-multiselect-lookup>
+            </div>
+        `,
+
+        methods: {
+            async onInput(value) {
+                const data = await this.handler.onCategoryInput(
+                    value,
+                    this.model.input,
+                    this.type
+                );
+
+                if (data) {
+                    this.model.menuItems = data;
+                }
+            },
+
+            async onLoadMore() {
+                const results = await this.handler.onLoadMore(this.model, this.type);
+
+                if (results?.length) {
+                    this.model.menuItems.push(...results);
+                }
+            }
+        }
+    }
+}
