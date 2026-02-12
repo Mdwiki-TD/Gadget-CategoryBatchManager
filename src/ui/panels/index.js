ab2cc4090b08fdcf7e1ff0689f1aa9fb1b66beb9
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-02-12 09:57:41 +0300

    .

--- src/ui/panels/index.js
+++ src/ui/panels/index.js
@@ -5,9 +5,7 @@
  */
 export { default as CategoryInputsPanel } from './CategoryInputsPanel.js';
 export { default as ExecutePanel } from './ExecutePanel.js';
-export { default as ExecuteProgressPanel } from './ExecuteProgressPanel.js';
 export { default as FilesListPanel } from './FilesListPanel.js';
 export { default as MessageDisplayPanel } from './MessageDisplayPanel.js';
 export { default as PreviewPanel } from './PreviewPanel.js';
 export { default as SearchPanel } from './SearchPanel.js';
-export { default as SearchProgressPanel } from './SearchProgressPanel.js';
